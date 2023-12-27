c06587d86d70711b71636bc65b1cda0dce97e379
Evgeny Zelenin <evgeny.globe@yandex.ru>
2023-12-27 15:14:39 +0300

    Сделал часть теста

--- Test_reg_form.py
+++ Test_reg_form.py
@@ -1,5 +1,5 @@
 import pytest
-from selene import browser, by, command, have
+from selene import browser, command, have
 
 
 @pytest.fixture
@@ -22,6 +22,21 @@ def test_registration_form(browser_options):
     browser.element('[class="react-datepicker__month-select"]').send_keys('June')
     browser.element('[class="react-datepicker__year-select"]').send_keys('1995')
     browser.element('[aria-label="Choose Tuesday, June 13th, 1995"]').perform(command.js.click)
-    browser.element('[id="subjectsInput"]').type('Ma')
+    browser.element('[id="subjectsInput"]').type('Ma').press_tab()
+    browser.element('[id="hobbies-checkbox-1"]').perform(command.js.click)
+    browser.element('[id="currentAddress"]').type('Russia, Moscow')
+    browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
+    browser.element('#react-select-4-input').type('Agra').press_enter()
     browser.element('[id="submit"]').perform(command.js.click)
-    ...
+
+
+    browser.element('[class="table-dark]').all('td').even.should(have.exact_texts(
+        'Пользователь Тестовый',
+        'Test@gmail.com',
+        'Male',
+        '8005553535',
+        '13 June,1995',
+        'Maths',
+        'Music',
+        'Russia, Moscow',
+        'Uttar Pradesh Agra'))
