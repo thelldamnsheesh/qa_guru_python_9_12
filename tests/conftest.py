c50ce5dfc8e737622f1ae4ad98593ac7c705c76f
Evgeny Zelenin <evgeny.globe@yandex.ru>
2024-03-05 00:14:52 +0300

    Обновил тест

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,34 @@
 import pytest
-from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_options():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1440
-    browser.config.window_height = 1860
+from util import attach
 
-    yield
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser.config
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
