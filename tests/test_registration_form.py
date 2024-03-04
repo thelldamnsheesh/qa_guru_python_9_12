3f4526868fe9247aabad0b292d1b537124c0a013
Evgeny Zelenin <evgeny.globe@yandex.ru>
2024-03-04 23:09:28 +0300

    Обновил тест

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,37 +1,55 @@
 import os
-from selene import browser, command, have
+import allure
+from selene import browser, command, have, by
 
 
+@allure.title('Форма регистрации студента')
 def test_registration_form():
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').type('Пользователь')
-    browser.element('#lastName').type('Тестовый')
-    browser.element('#userEmail').type('Test@gmail.com')
-    browser.all('[for^=gender-radio-1]').element_by(have.exact_text('Male')).click()
-    browser.element('#userNumber').type('8005553535')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').send_keys('June')
-    browser.element('.react-datepicker__year-select').send_keys('1995')
-    browser.element('[aria-label="Choose Tuesday, June 13th, 1995"]').perform(command.js.click)
-    browser.element('#subjectsInput').type('Ma').press_tab()
-    browser.all('[for^=hobbies-checkbox-1]').element_by(have.exact_text('Sports')).click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('../resources/mem.jpg'))
-    browser.element('#currentAddress').type('Russia, Moscow')
-    browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
-    browser.element('#react-select-4-input').type('Agra').press_enter()
+    with allure.step('Открываем форму'):
+        browser.open('/automation-practice-form')
 
-    browser.driver.execute_script("document.body.style.zoom='75%'")
+    with allure.step('Вводим данные студента'):
+        browser.element('#firstName').type('Пользователь')
+        browser.element('#lastName').type('Тестовый')
+        browser.element('#userEmail').type('Test@gmail.com')
+        browser.all('[for^=gender-radio-1]').element_by(have.exact_text('Male')).click()
+        browser.element('#userNumber').type('8005553535')
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__month-select').click().element(
+            by.text('June')
+        ).click()
+        browser.element('.react-datepicker__year-select').click().element(
+            by.text('1995')
+        ).click()
+        browser.element('.react-datepicker__day--013').click()
 
-    browser.element('#submit').perform(command.js.click)
+    with allure.step('Выбираем предмет'):
+        browser.element('#subjectsInput').type('Ma').press_tab()
 
+    with allure.step('Отмечаем хобби'):
+        browser.all('[for^=hobbies-checkbox-1]').element_by(have.exact_text('Sports')).click()
 
-    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    browser.element('.modal-body').should(have.text('Пользователь Тестовый'))
-    browser.element('.modal-body').should(have.text('Test@gmail.com'))
-    browser.element('.modal-body').should(have.text('Male'))
-    browser.element('.modal-body').should(have.text('8005553535'))
-    browser.element('.modal-body').should(have.text('13 June,1995'))
-    browser.element('.modal-body').should(have.text('Maths'))
-    browser.element('.modal-body').should(have.text('Sports'))
-    browser.element('.modal-body').should(have.text('Russia, Moscow'))
-    browser.element('.modal-body').should(have.text('Uttar Pradesh Agra'))
+    with allure.step('Загружаем картинку'):
+        browser.element('#uploadPicture').send_keys(os.path.abspath('../resources/mem.jpg'))
+
+    with allure.step('Вводим адрес'):
+        browser.element('#currentAddress').type('Russia, Moscow')
+
+    with allure.step('Выбираем штат и город'):
+        browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
+        browser.element('#react-select-4-input').type('Agra').press_enter()
+
+    with allure.step('Нажимаем на кнопку "Submit"'):
+        browser.element('#submit').perform(command.js.click)
+
+    with allure.step('Проверяем введенные данные'):
+        browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+        browser.element('.modal-body').should(have.text('Пользователь Тестовый'))
+        browser.element('.modal-body').should(have.text('Test@gmail.com'))
+        browser.element('.modal-body').should(have.text('Male'))
+        browser.element('.modal-body').should(have.text('8005553535'))
+        browser.element('.modal-body').should(have.text('13 June,1995'))
+        browser.element('.modal-body').should(have.text('Maths'))
+        browser.element('.modal-body').should(have.text('Sports'))
+        browser.element('.modal-body').should(have.text('Russia, Moscow'))
+        browser.element('.modal-body').should(have.text('Uttar Pradesh Agra'))
