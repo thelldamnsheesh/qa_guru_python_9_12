b708b37150022a1c168ff1e14b399aa485d0c362
Evgeny Zelenin <evgeny.globe@yandex.ru>
2024-03-05 17:44:02 +0300

    внес правки в фикстуры

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,11 @@ def setup_browser(request):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-setuid-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--incognito')
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
