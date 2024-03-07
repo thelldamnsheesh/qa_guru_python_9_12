31d8e5e537b3699ecf5e0ac8c4c7fc4c730ffed2
Evgeny Zelenin <evgeny.globe@yandex.ru>
2024-03-07 14:14:37 +0300

    фикс

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,13 @@
 import pytest
-from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
 from util import attach
 
-
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='function')
 def setup_browser(request):
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
     options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-setuid-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--incognito')
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
@@ -30,8 +22,10 @@ def setup_browser(request):
         options=options
     )
 
-    browser.config.driver = driver
-    yield
+    browser = Browser(Config(driver))
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield browser
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
